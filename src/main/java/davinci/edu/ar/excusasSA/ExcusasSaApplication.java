8bb356a5cb39f7165b0f8fa7fab04bce108956f0
yachela <laura.yachelini@davinci.edu.ar>
2025-11-11 08:18:10 -0300

    First commit

---
+++ src/main/java/davinci/edu/ar/excusasSA/ExcusasSaApplication.java
@@ -0,0 +1,13 @@
+package davinci.edu.ar.excusasSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcusasSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcusasSaApplication.class, args);
+	}
+
+}
