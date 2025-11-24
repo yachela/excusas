861f6f426008b796376e9453f206395a3591900c
yachela <laura.yachelini@davinci.edu.ar>
2025-11-24 07:20:47 -0300

    Agregar modelos Receptionist/InCharge

--- src/main/java/davinci/edu/ar/excusasSA/model/inchargers/Receptionist.java
+++ src/main/java/davinci/edu/ar/excusasSA/model/inchargers/Receptionist.java
@@ -1,4 +1,17 @@
 package davinci.edu.ar.excusasSA.model.inchargers;
 
-public class Receptionist {
+import davinci.edu.ar.excusasSA.model.strategy.Strategy;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("RECEPTIONIST")
+public class Receptionist extends InCharge {
+
+    public Receptionist() {
+    }
+
+    public Receptionist(String name, String email, Long legajo, Strategy strategy) {
+        super(name, email, legajo, strategy);
+    }
 }
