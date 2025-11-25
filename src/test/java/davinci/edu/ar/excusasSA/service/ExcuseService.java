c87c9623acad637360587346dfe6e02b4091f9f0
yachela <laura.yachelini@davinci.edu.ar>
2025-11-25 08:40:02 -0300

    Agregar meotodo para ejecutar la funcion de armar chain of responsability

--- src/test/java/davinci/edu/ar/excusasSA/service/ExcuseService.java
+++ src/test/java/davinci/edu/ar/excusasSA/service/ExcuseService.java
@@ -1,4 +1,30 @@
 package davinci.edu.ar.excusasSA.service;
 
+import davinci.edu.ar.excusasSA.repository.EmployeeRepository;
+import davinci.edu.ar.excusasSA.repository.ExcuseRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ExcuseService {
+
+    private final ExcuseRepository excuseRepository;
+    private final EmployeeRepository employeeRepository;
+    private Incharge chainHead;
+
+    @Autowired
+    public ExcuseService(ExcuseRepository excuseRepository, EmployeeRepository employeeRepository) {
+        this.excuseRepository = excuseRepository;
+        this.employeeRepository = employeeRepository;
+    }
+
+    @PostConstruct
+    public void initChain() {
+        if (employeeRepository.count() == 0) {
+            crearEncargadosDefault();
+        }
+
+        buildChainInMemory();
+    }
 }
