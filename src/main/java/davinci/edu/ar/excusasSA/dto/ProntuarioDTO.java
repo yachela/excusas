ca75ef2f30ee438d2e48591d51ea8229b1143950
yachela <laura.yachelini@davinci.edu.ar>
2025-11-26 09:00:28 -0300

    Agregar constructor vacio de prueba

--- src/main/java/davinci/edu/ar/excusasSA/dto/ProntuarioDTO.java
+++ src/main/java/davinci/edu/ar/excusasSA/dto/ProntuarioDTO.java
@@ -6,4 +6,7 @@ public class ProntuarioDTO {
     private String fecha;
     private String estadoExcusa;
     private String motivo;
+
+    public ProntuarioDTO(String name, String string, String status, String s) {
+    }
 }
