82d6ac972279c363b7822fe50e5652d3a11fdd77
yachela <laura.yachelini@davinci.edu.ar>
2025-12-02 08:13:07 -0300

    Agregar deto de ErrorDetails para mejorar el formato del error que se muestra

---
+++ src/main/java/davinci/edu/ar/excusasSA/exception/DuplicateResourceException.java
@@ -0,0 +1,4 @@
+package davinci.edu.ar.excusasSA.exception;
+
+public class DuplicateResourceException {
+}
---
+++ src/main/java/davinci/edu/ar/excusasSA/exception/ErrorDetails.java
@@ -0,0 +1,16 @@
+package davinci.edu.ar.excusasSA.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
