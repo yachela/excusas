8a1a4d52118091157735f14c08bc4107f1a4fee5
yachela <laura.yachelini@davinci.edu.ar>
2025-12-02 08:09:47 -0300

    Agregamos un método para buscar todas los prontuarios de un empleado específico.

--- src/main/java/davinci/edu/ar/excusasSA/repository/ProntuarioRepository.java
+++ src/main/java/davinci/edu/ar/excusasSA/repository/ProntuarioRepository.java
@@ -1,8 +1,14 @@
 package davinci.edu.ar.excusasSA.repository;
+
 import davinci.edu.ar.excusasSA.model.prontuario.Prontuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProntuarioRepository extends JpaRepository<Prontuario, Long> {
+
+    List<Prontuario> findByEmployeeId(Long employeeId);
 }
