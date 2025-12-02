494a649f14ce3aafe3b579c2fe2df05bc4480b5a
IKeViNI <flores.kevin2972@gmail.com>
2025-12-02 17:09:21 -0300

    refactor/Ajustar la implementación de InCharge y sus subtipos con test

--- src/main/java/davinci/edu/ar/excusasSA/model/employee/incharge/InCharge.java
+++ src/main/java/davinci/edu/ar/excusasSA/model/employee/incharge/InCharge.java
@@ -23,6 +23,7 @@ public abstract class InCharge extends Employee implements Handler {
     protected Strategy strategy;
     @Transient
     protected Handler next;
+    @Transient
     protected EmailSenderService emailSender;
 
     protected InCharge(String name, String email, Long legajo, Strategy strategy) {
