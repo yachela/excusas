51b08e0ab3bab5f1bc4224b7aad7ee2fb9807aaa
yachela <laura.yachelini@davinci.edu.ar>
2025-12-02 08:13:48 -0300

    Agregar Exception custom

--- src/main/java/davinci/edu/ar/excusasSA/exception/DuplicateResourceException.java
+++ src/main/java/davinci/edu/ar/excusasSA/exception/DuplicateResourceException.java
@@ -1,4 +1,7 @@
 package davinci.edu.ar.excusasSA.exception;
 
-public class DuplicateResourceException {
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
 }
