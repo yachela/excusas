e76949067a29dc88f20b6299c3162ce46234b264
yachela <laura.yachelini@davinci.edu.ar>
2025-12-02 08:08:56 -0300

    Agregamos un método para buscar todas las excusas de un empleado específico.

--- src/main/java/davinci/edu/ar/excusasSA/repository/ExcuseRepository.java
+++ src/main/java/davinci/edu/ar/excusasSA/repository/ExcuseRepository.java
@@ -1,8 +1,15 @@
 package davinci.edu.ar.excusasSA.repository;
+
 import davinci.edu.ar.excusasSA.model.excuse.Excuse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExcuseRepository extends JpaRepository<Excuse, Long> {
+
+    List<Excuse> findByEmployeeId(Long employeeId);
+
+    // List<Excuse> findByStatus(ExcuseStatus status);
 }
